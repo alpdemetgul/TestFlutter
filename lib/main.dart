8d50a460bb39e1bac284c2ed920c4e87fdd0aa29
Adylsha Yumayev <adylshay@gmail.com>
2023-04-12 15:00:23 +0300

    zort

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_colarx/autofill.dart';
 import 'package:flutter_colarx/biometrics.dart';
@@ -11,11 +12,21 @@ import 'CameraPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.instance
+      .getToken()
+      .then((value) => {print('FCM Token:  ' + value.toString())});
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(MyApp());
 }
 
